e4c2fa591ce16518dea2d182572d0b66a00673d0
mgwoiii <mgwoiii@naver.com>
2022-02-13 22:38:32 +0900

    초난감 Dao 설정

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,77 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	
+	public static void main(String[] args ) throws ClassNotFoundException, SQLException{
+		
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("whiteship2");
+		user.setName("백기선");
+		user.setPassword("married");
+		
+		dao.add(user);
+		
+		System.out.println("성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + "조회 성공");
+	}
+	
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		// Class.forName("com.mysql.cj.jdbc.Driver");
+		////Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/springbook?useSSL=false;allowPublicKeyRetrieval=true&amp;serverTimezone=UTC", "root", "1234");
+		// Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/springbook?serverTimezone=UTC", "root", "1234");
+		
+		Connection c = getConnection();
+		
+		
+		PreparedStatement ps = c.prepareStatement(
+				"insert into users(id, name, password) values(?,?,?)");
+				ps.setString(1, user.getId());
+				ps.setString(2, user.getName());
+				ps.setString(3, user.getPassword());
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/springbook?serverTimezone=UTC", "root", "1234");
+
+		PreparedStatement ps = c.prepareStatement(
+				"select * from users where id = ?");
+		
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+}
