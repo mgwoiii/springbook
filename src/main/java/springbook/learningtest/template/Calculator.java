1c637689d85781789c8fdfcbf81e226cfd654d7e
mgwoiii <mgwoiii@naver.com>
2022-03-30 23:32:10 +0900

    템플릿 콜백 실습

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -8,14 +8,76 @@ public class Calculator {
 
 	public Integer calcSum(String filepath) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new FileReader(filepath));
-		Integer sum = 0;
-		String line = null;
-		while((line = br.readLine()) != null) {
-			sum += Integer.valueOf(line);
-		}
-		
-		br.close();
-		return sum;
+		LineCallback sumCallback = 
+				new LineCallback() {
+	
+			public Integer doSomethingWithLine(String line, Integer value) {
+				// TODO Auto-generated method stub
+				return value + Integer.valueOf(line);
+			}
+		};
+		return lineReadTemplate(filepath, sumCallback, 0);
+
+
 	}
+	
+	public Integer calcMultiply(String filepath) throws IOException{
+		LineCallback multiplyCallback = 
+				new LineCallback() {
+	
+			public Integer doSomethingWithLine(String line, Integer value) {
+				// TODO Auto-generated method stub
+				return value * Integer.valueOf(line);
+			}
+		};
+		return lineReadTemplate(filepath, multiplyCallback , 0);
+	}
+	
+
+	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException{
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			Integer res = initVal;
+			String line = null;
+			
+			while((line = br.readLine()) != null) {
+				res = callback.doSomethingWithLine(line, res);
+			}
+			return res;
+		}catch(IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+		finally {
+			if( br != null) {
+				try { br.close();}
+				catch (IOException e) {System.out.println(e.getMessage());}
+			}
+		}
+	}
+
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			int ret = callback.doSomethingWithReader(br);
+			
+			return ret;
+			
+		}catch(IOException e) {
+			
+			System.out.println(e.getMessage());
+			throw e;
+			
+		}finally {
+			if( br != null) {
+				try { br.close();}
+				catch (IOException e) {System.out.println(e.getMessage());}
+			}
+		}
+	}
+	
 }
